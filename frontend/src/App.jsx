547a1934c81fe2a77d55515464e53b5c557ad340
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-04-18 23:24:15 +0530

    Tailwind working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,12 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-9xl'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-2xl'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
